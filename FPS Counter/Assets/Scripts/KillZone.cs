6bdfa7e167a8119602a5b2cdc8fa738807230931
ChiefTripodBear <avalacos@gmail.com>
2017-10-12 17:06:47 +1100

    fps counter done and object pools wip

---
+++ FPS Counter/Assets/Scripts/KillZone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    void OnTriggerEnter(Collider enteredCollider)
+    {
+        if (enteredCollider.CompareTag("KillZone"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ FPS Counter/Assets/Scripts/StuffSpawner.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class StuffSpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public struct FloatRange
+    {
+        public float min, max;
+
+        public float RandomInRange
+        {
+            get
+            {
+                return Random.Range(min, max);
+            }
+        }
+    }
+
+    public float velocity;
+
+    public Stuff[] stuffPrefabs;
+
+    public FloatRange timeBetweenSpawns, scale, randomVelocity, angularVelocity;
+
+    float currentSpawnDelay;
+    float timeSinceLastSpawn;
+
+    void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= currentSpawnDelay)
+        {
+            currentSpawnDelay -= timeBetweenSpawns.RandomInRange;
+            SpawnStuff();
+        }
+    }
+
+    void SpawnStuff()
+    {
+        Stuff prefab = stuffPrefabs[Random.Range(0, stuffPrefabs.Length)];
+        Stuff spawn = Instantiate<Stuff>(prefab);
+        spawn.transform.localPosition = transform.position;
+        spawn.transform.localScale = Vector3.one * scale.RandomInRange;
+        spawn.transform.localRotation = Random.rotation;
+        spawn.Body.velocity = transform.up * velocity +
+        Random.onUnitSphere * randomVelocity.RandomInRange;
+        spawn.Body.angularVelocity = Random.onUnitSphere * angularVelocity.RandomInRange;
+    }
+}
